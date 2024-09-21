1a4de16f8dbce08f20933218b8020993e54320ad
aquanjsw <zhdlcc@gmail.com>
2024-09-21 10:21:27 +0800

    debug workflow

---
+++ build.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import subprocess
+from dataclasses import dataclass
+import yaml
+import argparse
+
+type Url = str
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ipk-dir', help='Output directory for ipk files')
+
+
+@dataclass
+class Package:
+    name: str
+    patches: list[Url]
+
+
+def load_packages():
+    db_path = 'build_db.yaml'
+    with open(db_path) as fs:
+        db = yaml.safe_load(fs)
+    return [Package(**pkg) for pkg in db]
+
+
+def main():
+    args = parser.parse_args()
+    packages = load_packages()
+    for pkg in packages:
+        pkg_dir = (
+            subprocess.run(
+                f'find feeds/ -name {pkg.name} -type d'.split(), capture_output=True
+            )
+            .stdout.decode()
+            .split()[0]
+        )
+        patch_dir = f'{pkg_dir}/patches'
+        for patch in pkg.patches:
+            subprocess.run(['wget', '-P', patch_dir, patch])
+        subprocess.run(f'make package/{pkg.name}/compile -j'.split())
+        subprocess.run(
+            f'find bin/ -name *{pkg.name}*.ipk -type f -exec cp {{}} {args.ipk_dir}'.split()
+        )
+
+
+if __name__ == '__main__':
+    main()
--- deploy_patches.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-wget -o feeds/packages/net/miniupnpd/patches/000-bypass-reserved-ip.patch https://github.com/AquanJSW/miniupnpd/releases/download/modify-2.3.3/000-bypass-reserved-ip.patch
