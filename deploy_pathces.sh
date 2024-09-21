dde1bfc7ff5e3c8a1b5130bd8644ba736bd039ed
aquanjsw <zhdlcc@gmail.com>
2024-09-21 08:58:26 +0800

    debug workflow

---
+++ deploy_pathces.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -o feeds/packages/net/miniupnpd/patches/000-bypass-reserved-ip.patch https://github.com/AquanJSW/miniupnpd/releases/download/modify-2.3.3/000-bypass-reserved-ip.patch
